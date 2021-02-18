ddfda6afd19f41609f4452ffd6221523b3d23329
Matheus Eyng de França <matheuseyngsf@gmail.com>
2021-02-18 18:30:22 -0300

    Add more helper functions

--- source/utils/Grammar.py
+++ source/utils/Grammar.py
@@ -1,10 +1,10 @@
 import json
 
+
 class Grammar:
 
     def __init__(self):
         self.dictionary = {}
-
 
     def from_file(self, file_path):
         with open(file_path) as f:
@@ -17,9 +17,8 @@ class Grammar:
 
                 self.dictionary[key] = value
 
-
     def to_file(self, file_path):
-        f = open("file_path", "w")
+        f = open(file_path, "w")
 
         for key in self.dictionary:
             write_string = key + " -> "
@@ -32,44 +31,45 @@ class Grammar:
                     write_string += " | " + value
             f.write(write_string + '\n')
 
-
     def from_json(self, filepath):
         self.dictionary = json.loads(filepath)
-
 
     def to_json(self):
         return json.dumps(self.dictionary, indent=2)
 
-
     def get_dictionary(self):
         return self.dictionary
-
 
     def get_variables(self):
         variables = list()
         for key in self.dictionary:
-            variables_head = list(filter(lambda x: x.isupper(), key))
-            for x in variables_head:
+            keys = list(filter(lambda k: k.isupper(), key))
+            for x in keys:
                 variables.append(x)
 
             for string in self.dictionary[key]:
-                variables_body = list(filter(lambda x: x.isupper(), string))
-                for x in variables_body:
+                values = list(filter(lambda v: v.isupper(), string))
+                for x in values:
                     variables.append(x)
 
         return list(set(variables))
-
 
     def get_terminals(self):
-        variables = list()
+        terminals = list()
         for key in self.dictionary:
-            variables_head = list(filter(lambda x: x.islower(), key))
-            for x in variables_head:
-                variables.append(x)
+            keys = list(filter(lambda k: k.islower(), key))
+            for x in keys:
+                terminals.append(x)
 
             for string in self.dictionary[key]:
-                variables_body = list(filter(lambda x: x.islower(), string))
-                for x in variables_body:
-                    variables.append(x)
+                values = list(filter(lambda v: v.islower(), string))
+                for x in values:
+                    terminals.append(x)
 
-        return list(set(variables))
+        return list(set(terminals))
+
+    def get_derivations(self, key):
+        return self.dictionary[key]
+
+    def add_key(self, key, values):
+        self.dictionary.Add(key, values)
