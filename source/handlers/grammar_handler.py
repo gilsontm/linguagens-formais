c3e2bc4c7bd1cb4c2602f0288496c7a558c30b6a
Matheus Eyng de França <matheuseyngsf@gmail.com>
2021-04-02 17:33:24 -0300

    Add export as afd buttons to grammar menu

--- source/handlers/grammar_handler.py
+++ source/handlers/grammar_handler.py
@@ -2,6 +2,7 @@ import requests
 from flask import *
 from utils.path import get_file_path
 from models.grammar import Grammar
+from models.grammar_converter import GrammarConverter
 
 
 blueprint = Blueprint("grammar", __name__)
@@ -24,4 +25,14 @@ class GrammarHandler:
         grammar = Grammar()
         grammar.from_json(request.get_json())
         grammar.to_file(path)
+        return send_file(path, mimetype="text/plain")
+
+    @blueprint.route("/export-afd", methods=["POST"])
+    def export_grammar():
+        path = get_file_path("grammar.txt")
+        grammar = Grammar()
+        converter = GrammarConverter()
+        grammar.from_json(request.get_json())
+        converter.grammar_to_automata(grammar)
+        converter.automata.to_file(path)
         return send_file(path, mimetype="text/plain")
