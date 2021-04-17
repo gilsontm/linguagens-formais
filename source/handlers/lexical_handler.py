0bfc2bc94b1c9a67eebb3ebe8fca96c9761c6f76
Gilson <gilson.t.magro@gmail.com>
2021-04-17 20:54:35 -0300

    Criação de analisador léxico conectada ao backend
    - A criação de analisador léxico funciona via interface.

--- source/handlers/lexical_handler.py
+++ source/handlers/lexical_handler.py
@@ -3,6 +3,7 @@ from flask import *
 from utils import messages
 from utils.path import get_file_path
 from exceptions.invalid_usage import InvalidUsage
+from models.lexical_analyzer import LexicalAnalyzer
 
 
 blueprint = Blueprint("lexical", __name__)
@@ -11,9 +12,10 @@ class LexicalHandler:
 
     @blueprint.route("/create-analyzer", methods=["POST"])
     def build_analyzer():
-        # TODO: ler regexes e criar analisador léxico
-        # TODO: como salvar analisador entre requisições?
-        pass
+        path = get_file_path("analyzer.txt")
+        analyzer = LexicalAnalyzer.from_json(request.get_json())
+        analyzer.to_file(path)
+        return send_file(path, mimetype="text/plain")
 
     @blueprint.route("/analyze", methods=["POST"])
     def analyze():
